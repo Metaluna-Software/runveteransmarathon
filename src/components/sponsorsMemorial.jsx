1f655b2c0efafb2eef733d054f80762b03e00e3d
Letter3 <seth@sethrgbird.com>
2022-05-26 11:23:04 -0500

    Add mercury broadband logo

--- src/components/sponsorsMemorial.jsx
+++ src/components/sponsorsMemorial.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 const SponsorsMemorial = () => {
-  const interval = 3000
-  const controls = true
+  const interval = 3000;
+  const controls = true;
 
-  return (
-    <div className='thank-you-heading h2'><span>Thank you to our sponsors</span>
-      <Carousel controls={controls} interval={interval}>
-        <Carousel.Item>
-          <img className='d-block w-90' src='../images/sponsors/logo_cricket-green.webp' alt='cricket wireless'/>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className='d-block w-90' src='../images/sponsors/culligan.PNG' alt='culligan'/>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className='d-block w-90' src='../images/sponsors/mooover.jpg' alt='Moo Over'/>
-        </Carousel.Item>
-      </Carousel>
-    </div>
-  );
+  return (<div className="thank-you-heading h2"><span>Thank you to our sponsors</span>
+    <Carousel controls={controls} interval={interval}>
+      <Carousel.Item>
+        <img className="d-block w-90" src="../images/sponsors/logo_cricket-green.webp" alt="cricket wireless"/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-90" src="../images/sponsors/culligan.PNG" alt="culligan"/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-90" src="../images/sponsors/mooover.jpg" alt="Moo Over"/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-90" src="../images/sponsors/mercurybroadband.svg" alt="Mercury Broadband"/>
+      </Carousel.Item>
+    </Carousel>
+  </div>);
 };
 
 export default SponsorsMemorial;
