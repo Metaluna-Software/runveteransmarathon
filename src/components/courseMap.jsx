ec78ddcebadba95cddb1acd3bb694d00d7f073a1
Letter3 <seth@sethrgbird.com>
2023-10-17 20:24:55 -0500

    Update 5k map

--- src/components/courseMap.jsx
+++ src/components/courseMap.jsx
@@ -11,7 +11,7 @@ const CourseMap = (props) => {
     <div>
       <h2>Course map</h2>
       <h4>If the map doesn't load, <a href={enJson.course[course+"_link"]} target="_blank">click here</a></h4>
-      <iframe loading={"lazy"} id="mapmyfitness_route" src={enJson.course[course]} height="660px" width="100%"/>
+      <iframe loading={"lazy"} id="mapmyfitness_route" src={enJson.course[course]} height="685px" width="100%"/>
     </div>
   );
 };
