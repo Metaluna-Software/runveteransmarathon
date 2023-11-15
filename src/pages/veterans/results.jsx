f54e5bf99e88190fb6dd6417ac39dd9d3ecb458f
Letter3 <seth@sethrgbird.com>
2023-11-14 20:35:33 -0600

    Update results.jsx

--- src/pages/veterans/results.jsx
+++ src/pages/veterans/results.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import Sponsors from '../../components/sponsors';
 
 const yearOptions = [
+  { value: '2023', label: '2023' },
   { value: '2022', label: '2022' },
   { value: '2021', label: '2021' },
   { value: '2020', label: '2020' },
